58d1e879e76ab6eb82a8772197f34663f143c93d
muhammadijlan34-prog <muhammadijlan34@gmail.com>
2026-07-31 21:17:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -62,3 +62,18 @@ if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("service-worker.js");
   });
 }
+let deferredPrompt;
+const installBtn = document.getElementById("installBtn");
+
+window.addEventListener("beforeinstallprompt", (e)=>{
+    e.preventDefault();
+    deferredPrompt = e;
+    installBtn.style.display = "block";
+});
+
+installBtn.addEventListener("click", async ()=>{
+    installBtn.style.display = "none";
+    deferredPrompt.prompt();
+    await deferredPrompt.userChoice;
+    deferredPrompt = null;
+});
