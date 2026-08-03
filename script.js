ed6ec556a3ad51775c0c472214f0ffd34af2dc5b
muhammadijlan34-prog <muhammadijlan34@gmail.com>
2026-08-03 21:25:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -184,3 +184,12 @@ document.querySelectorAll(".year").forEach((year) => {
     year.textContent = new Date().getFullYear();
 
 });
+const menuBtn=document.getElementById("menuBtn");
+
+const navLinks=document.getElementById("navLinks");
+
+menuBtn.addEventListener("click",()=>{
+
+    navLinks.classList.toggle("active");
+
+});
